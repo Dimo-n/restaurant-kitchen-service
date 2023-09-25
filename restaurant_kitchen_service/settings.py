c02e2f86a038311ab28f8630d954526b01fe060a
Dimo-n <dimon.chmara@gmail.com>
2023-09-25 09:22:32 +0300

    Add kitchen-service-mate.onrender.com to ALLOWED_HOSTS

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # DEBUG = False
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "kitchen-service-mate.onrender.com"
+]
 
 
 # Application definition
